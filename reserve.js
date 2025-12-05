26b0252f1426fc6cef71a4ff221450f0aaa087ac
samst3r <samuelshahzaman@gmail.com>
2025-12-05 12:18:42 -0500

    fixed some changed with the form

--- reserve.js
+++ reserve.js
@@ -16,57 +16,56 @@ animate("#form", {
 
 document.addEventListener("DOMContentLoaded", (event) => {
   form.addEventListener("submit", getFormData);
-
-  let reservationTimes = [
-    "9:00 AM",
-    "9:15 AM",
-    "9:30 AM",
-    "9:45 AM",
-    "10:00 AM",
-    "10:15 AM",
-    "10:30 AM",
-    "10:45 AM",
-    "11:00 AM",
-    "11:15 AM",
-    "11:30 AM",
-    "11:45 AM",
-    "12:00 PM",
-    "12:15 PM",
-    "12:30 PM",
-    "12:45 PM",
-    "1:00 PM",
-    "1:15 PM",
-    "1:30 PM",
-    "1:45 PM",
-    "2:00 PM",
-    "2:15 PM",
-    "2:30 PM",
-    "2:45 PM",
-    "3:00 PM",
-    "3:15 PM",
-    "3:30 PM",
-    "3:45 PM",
-    "4:00 PM",
-    "4:15 PM",
-    "4:30 PM",
-    "4:45 PM",
-    "5:00 PM",
-    "5:15 PM",
-    "5:30 PM",
-    "5:45 PM",
-    "6:00 PM",
-    "6:15 PM",
-    "6:30 PM",
-    "6:45 PM",
-    "7:00 PM",
-    "7:15 PM",
-    "7:30 PM",
-    "7:45 PM",
-    "8:00 PM",
-  ];
   timeInit();
 });
-
+let initalTime = false;
+let reservationTimes = [
+  "9:00 AM",
+  "9:15 AM",
+  "9:30 AM",
+  "9:45 AM",
+  "10:00 AM",
+  "10:15 AM",
+  "10:30 AM",
+  "10:45 AM",
+  "11:00 AM",
+  "11:15 AM",
+  "11:30 AM",
+  "11:45 AM",
+  "12:00 PM",
+  "12:15 PM",
+  "12:30 PM",
+  "12:45 PM",
+  "1:00 PM",
+  "1:15 PM",
+  "1:30 PM",
+  "1:45 PM",
+  "2:00 PM",
+  "2:15 PM",
+  "2:30 PM",
+  "2:45 PM",
+  "3:00 PM",
+  "3:15 PM",
+  "3:30 PM",
+  "3:45 PM",
+  "4:00 PM",
+  "4:15 PM",
+  "4:30 PM",
+  "4:45 PM",
+  "5:00 PM",
+  "5:15 PM",
+  "5:30 PM",
+  "5:45 PM",
+  "6:00 PM",
+  "6:15 PM",
+  "6:30 PM",
+  "6:45 PM",
+  "7:00 PM",
+  "7:15 PM",
+  "7:30 PM",
+  "7:45 PM",
+  "8:00 PM",
+];
 function timeInit() {
   for (let t = 0; t < reservationTimes.length; t++) {
     const option = document.createElement("option");
@@ -79,6 +78,7 @@ function timeInit() {
     const selection = document.querySelector("#timeInput");
     selection.add(option);
   }
+  initalTime = true;
 }
 
 document.querySelector("#dateInput").min = new Date()
@@ -120,9 +120,3 @@ $("#popup-button").on("click", function () {
   console.log("clicked");
   $(".popup-confirmation").fadeOut("slow");
 });
-
-let dateInput = document.querySelector("#dateInput");
-
-dateInput.addEventListener("oninput", dateReset());
-
-function dateReset() {}
